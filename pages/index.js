6328fe6fb5ce93e0b07990fbcf501fbbf5ca74ef
robrunodev <ro_web1@hotmail.com>
2025-01-06 08:30:21 -0300

    adding lint:check and lint:fix scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Hello World 💻</h1>
+  return <h1>Hello World 💻</h1>;
 }
 
-export default Home
+export default Home;
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,3 +1,9 @@
-export default function Products() {
-  return <h1>Products</h1>
+function Products() {
+  return <h1>Products</h1>;
 }
+
+function teste() {
+  console.log("teste")
+}
+
+export default Products
