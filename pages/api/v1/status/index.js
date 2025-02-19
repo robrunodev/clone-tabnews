bc8aa8d9020835544cb3d31130bd8b474463c17e
robrunodev <ro_web1@hotmail.com>
2025-02-19 07:59:11 -0300

    refactor database configuration to use environment variables and clean up code

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,6 +2,5 @@ import database from "../../../../infra/database";
 
 export default async function status(req, res) {
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
   return res.status(200).json({ chave: "Olá mundo, api status" });
 }
