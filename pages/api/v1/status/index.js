c6626e3b89238e92acf57447e87ffbad524deb52
robrunodev <ro_web1@hotmail.com>
2025-03-05 09:54:15 -0300

    enhance API status endpoint to return PostgreSQL version and connection statistics

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,23 @@
 import database from "infra/database";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  return res.status(200).json({ chave: "Olá mundo, api status" });
+  const postrgresVersion = await database.query(
+    "SELECT SETTING from pg_settings where NAME = 'server_version';",
+  );
+  const databaseMaxConnections = await database.query(
+    "SELECT SETTING from pg_settings where NAME = 'max_connections';",
+  );
+  const usedConnections = await database.query(
+    "SELECT numbackends from pg_stat_database WHERE datname = 'local_db';",
+  );
+
+  const updatedAt = new Date().toISOString();
+  return res.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      postgres_version: Number(postrgresVersion.rows[0].setting),
+      database_max_connections: Number(databaseMaxConnections.rows[0].setting),
+      used_connections: Number(usedConnections.rows[0].numbackends),
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET to /api/vi/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const { updated_at } = responseBody;
+
+  const parsedUpdatedAt = new Date(updated_at).toISOString();
+  expect(updated_at).toBeDefined();
+  expect(updated_at).toEqual(parsedUpdatedAt);
+  console.log({ responseBody });
 });
