ae2ab3f5a06c74b6eed3ccaa01ccc6452aa597d5
robrunodev <ro_web1@hotmail.com>
2025-04-17 08:02:56 -0300

    refactor SSL configuration in database connection for improved clarity

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,14 @@
 import { Client } from "pg";
 
+const getSSlValue = () => {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+};
+
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -7,7 +16,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSlValue(),
   });
 
   try {
